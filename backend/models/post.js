daf3b9738224dda8a8c6fd7f2622cde51d989f24
Audrey Rannou <audrey.rannou@gmail.com>
2022-08-02 11:08:45 +0200

    remise en place image true model post

--- backend/models/post.js
+++ backend/models/post.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const postSchema = mongoose.Schema({
   userId: { type: String, required: true },
   message: { type: String, required: true },
-  imageUrl: { type: String, required: true },
+  imageUrl: { type: String, required: true},
   likes: { type: Number, required: true },
   usersLiked: { type: Array, required: true },
   // username: { type: String, required: true, },
