53f53fe554eb091fb98f97e1ca1a6d7344b23e55
Audrey Rannou <audrey.rannou@gmail.com>
2022-08-09 10:18:13 +0200

    titres pages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,33 +8,50 @@ import createPost from "@/views/createPost.vue";
 
 
 const routes = [
-  {
-    name: "Login",
-    path: "/login",
-    component: Login,
-  },
+
+
   {
     name: "Home",
     path: "/",
     component: Home,
-    props: true,
+    meta: {
+      title : 'home'
+    }
   },
+  {
+    name: "Login",
+    path: "/login",
+    component: Login,
+    meta : {
+      title : 'connexion'
+    }
+  },
+  
   {
     name: "Publish",
     path: "/publish",
     component: createPost,
     props: true,
+    meta : {
+      title: 'publish'
+    }
   },
   {
     name: "Modifier",
     path: "/modifier",
     component: ModifyPost,
+    meta : {
+      title: 'update'
   },
+}
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+router.afterEach((to) =>{
+  document.title = to.meta.title;
+});
 
 export default router;
